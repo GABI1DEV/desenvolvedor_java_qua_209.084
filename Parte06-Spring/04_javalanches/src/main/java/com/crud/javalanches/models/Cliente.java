8d40128b9c6b77f2b6b70d5d96545a48fd0f2fc9
GABI1DEV <sgabriela.dev@gmail.com>
2026-05-19 21:41:53 -0300

    lanch

---
+++ Parte06-Spring/04_javalanches/src/main/java/com/crud/javalanches/models/Cliente.java
@@ -0,0 +1,24 @@
+package com.crud.javalanches.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Cliente {
+
+    //ATRIBUTOS
+
+    private long codigo_cliente;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private LocalDate dataNascimento;
+
+    public Cliente(){
+        
+    }
+
+
+}
